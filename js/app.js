12730d5b3f3ab1fef50421e0048a062b5e40095c
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-14 19:17:00 -0400

    feat. styling the images and the nav bar

--- js/app.js
+++ js/app.js
@@ -1,25 +1,9 @@
 //Prints value of x into console
 
-const el = document.getElementsByTagName("input")
+const el = document.getElementsByTagName("submit")
 console.log(el)
 el[1].addEventListener("click", function(event){
     event.preventDefault()
     console.log(el[0].value)
     alert("Please enter a valid e-mail address")
 })
-
-const num1 = 5
-const num2 = 10
-const num3 = 7
-const num4 = 8
-
-
-if (num1 > num2) {
-    console.log(`${num1} is less than ${num2}`)
-} else if (num1 === num2) {
-    console.log(`${num1} is equal than ${num2}`)
-} else {
-    console.log(`${num1} is greater than ${num2}`)
-}
-
-num1 < num2 ? console.log("true") : console.log("false")
