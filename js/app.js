b31160381664f73cd2bf27862fe67f85912aa7c5
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-15 13:54:40 -0400

    feat. smooth scroll add on

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-//Prints value of x into console
+// const el = document.getElementsByTagName("submit")
+// console.log(el)
+// el[1].addEventListener("click", function(event){
+//     event.preventDefault()
+//     console.log(el[0].value)
+//     alert("Thank you for subscribing")
+// })
 
-const el = document.getElementsByTagName("submit")
-console.log(el)
-el[1].addEventListener("click", function(event){
-    event.preventDefault()
-    console.log(el[0].value)
-    alert("Please enter a valid e-mail address")
-})
+
+document.getElementById("submit").onclick = function() {subscribeButton()};
+
+function subscribeButton() {
+  document.getElementById("submit")
+  alert("Thank you for subscribing")
+}
